e001bd0daefc197d7a8b4440541a42fba209649c
Juan Pablo <juanpapabon@gmail.com>
2022-04-12 08:32:38 -0500

    refactor: 💡 register listener in provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Listeners\RegisteredEvent;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 use App\Models\User;
@@ -16,12 +17,16 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Registered::class => [
+            RegisteredEvent::class,
+        ],
     ];
 
     public function boot(): void
     {
         User::observe(ModelObserver::class);
         Product::observe(ModelObserver::class);
+        parent::boot();
     }
 
     public function shouldDiscoverEvents(): bool
