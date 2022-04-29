813dbe603b51faebf3f4decea95323d3f6c505bb
Juan Pablo <juanpapabon@gmail.com>
2022-04-29 12:53:12 -0500

    feat: 🎸 define the route and option to export users

--- routes/export.php
+++ routes/export.php
@@ -1,15 +1,11 @@
 <?php
 
-use App\Http\Controllers\ExportProductController;
-
-//Route::get('products/export/', [ExportProductController::class, 'export']);
-
 Route::get('products/export/', [
     'as' => 'products.export',
     'uses' => 'ExportProductController@export',
 ]);
 
-/*Route::get('pay/payAgain/{reference?}', [
-    'as' => 'pay.payAgain',
-    'uses' => 'Admin\AdminPayController@payAgain',
-]);*/
+Route::get('users/export/', [
+    'as' => 'users.export',
+    'uses' => 'ExportUserController@export',
+]);
