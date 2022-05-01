bcc7ab4504669dc7406c254cd25a9d0362bb2a51
Juan Pablo <juanpapabon@gmail.com>
2022-05-01 01:22:38 -0500

    feat: 🎸 define the route to import categories from excel

--- routes/import.php
+++ routes/import.php
@@ -4,3 +4,8 @@ Route::post('products/import/', [
     'as' => 'products.import',
     'uses' => 'Imports\ImportProductController@import',
 ]);
+
+Route::post('categories/import/', [
+    'as' => 'categories.import',
+    'uses' => 'Imports\ImportCategoryController@import',
+]);
