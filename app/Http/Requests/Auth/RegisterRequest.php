c088d47d4157d81fe71488e4febe690dbc569f41
Juan Pablo <juanpapabon@gmail.com>
2022-05-14 23:53:17 -0500

    refactor: 💡 add validations to the authentication of the API

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100'],
+            'surname' => ['required', 'string', 'max:100'],
+            'identification' => ['required', 'string', 'min:8', 'max:10', 'unique:users'],
+            'address' => ['required', 'string', 'max:100'],
+            'phone' => ['required', 'string', 'min:10', 'max:10'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+}
