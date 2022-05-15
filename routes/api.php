510496f388615e712fdb82b83da7bd82738ff573
Juan Pablo <juanpapabon@gmail.com>
2022-05-14 23:49:49 -0500

    feat: 🎸 define the routes to manage products from REST API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('api/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('api/login', [\App\Http\Controllers\Api\Auth\AuthController::class, 'login']);
+
+Route::post('api/register', [\App\Http\Controllers\Api\Auth\AuthController::class, 'register']);
+
+Route::post('api/logout', [\App\Http\Controllers\Api\Auth\AuthController::class, 'logout']);
+
+Route::get('api/products/index', [
+    'as' => 'api.products.index',
+    'uses' => 'Api\ApiProductController@index',
+]);
+
+Route::get('api/product/show/{product}', [
+    'as' => 'api.product.show',
+    'uses' => 'Api\ApiProductController@show',
+]);
+
+Route::post('api/product/store', [
+    'as' => 'api.product.store',
+    'uses' => 'Api\ApiProductController@store',
+]);
+
+Route::put('api/product/update/{product}', [
+    'as' => 'api.product.update',
+    'uses' => 'Api\ApiProductController@update',
+]);
+
+Route::get('api/product/delete/{product}', [
+    'as' => 'api.product.delete',
+    'uses' => 'Api\ApiProductController@destroy',
+]);
