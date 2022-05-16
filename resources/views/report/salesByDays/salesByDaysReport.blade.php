a524ed7867cf8db579b97d3417953110e78c7549
Juan2004Pablo <95164848+Juan2004Pablo@users.noreply.github.com>
2022-05-16 08:19:01 -0500

    Refactor: update sales by day report

--- resources/views/report/salesByDays/salesByDaysReport.blade.php
+++ resources/views/report/salesByDays/salesByDaysReport.blade.php
@@ -20,12 +20,12 @@
 
                 <tr>
 
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.id') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.day') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.date') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.code') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.status') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.total') }}</th>
+                    <th class="p-3" id="i">{{ trans('report.sales_by_days_report.fields.id') }}</th>
+                    <th class="p-3" id="da">{{ trans('report.sales_by_days_report.fields.day') }}</th>
+                    <th class="p-3" id="dat">{{ trans('report.sales_by_days_report.fields.date') }}</th>
+                    <th class="p-3" id="cod">{{ trans('report.sales_by_days_report.fields.code') }}</th>
+                    <th class="p-3" id="statu">{{ trans('report.sales_by_days_report.fields.status') }}</th>
+                    <th class="p-3" id="tot">{{ trans('report.sales_by_days_report.fields.total') }}</th>
                     
                 </tr>
 
@@ -39,12 +39,12 @@
 
                     <tr class="bg-primary my-3 rounded">
 
-                        <th class="p-3">{{ $salesByDay->id }}</th>
-                        <th class="p-3">{{ $day[$loop->index] }}</th>
-                        <th class="p-3">{{ \Carbon\Carbon::parse($salesByDay->created_at)->format('d/m/Y') }}</th>
-                        <th class="p-3">{{ $salesByDay->code }}</th>
-                        <th class="p-3">{{ $salesByDay->status }}</th>
-                        <th class="p-3">COP {{ number_format($salesByDay->total, 0) }}</th>
+                        <th class="p-3" id="id">{{ $salesByDay->id }}</th>
+                        <th class="p-3" id="day">{{ $day[$loop->index] }}</th>
+                        <th class="p-3" id="date">{{ \Carbon\Carbon::parse($salesByDay->created_at)->format('d/m/Y') }}</th>
+                        <th class="p-3" id="code">{{ $salesByDay->code }}</th>
+                        <th class="p-3" id="status">{{ $salesByDay->status }}</th>
+                        <th class="p-3" id="total">COP {{ number_format($salesByDay->total, 0) }}</th>
 
                     </tr>
 
@@ -55,6 +55,8 @@
         </table>
 
         <table class="table table-striped">
+        
+            <caption>{{ trans('report.sales_by_days_report.title.title') }}</caption>
 
             <br><br>
 
@@ -62,10 +64,10 @@
 
                 <tr>
 
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.day_of_week') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.accumulated') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.growth') }}</th>
-                    <th class="p-3">{{ trans('report.sales_by_days_report.fields.previus') }}</th>
+                    <th class="p-3" id="dayweek">{{ trans('report.sales_by_days_report.fields.day_of_week') }}</th>
+                    <th class="p-3" id="accumulated">{{ trans('report.sales_by_days_report.fields.accumulated') }}</th>
+                    <th class="p-3" id="growth">{{ trans('report.sales_by_days_report.fields.growth') }}</th>
+                    <th class="p-3" id="previus">{{ trans('report.sales_by_days_report.fields.previus') }}</th>
                     
                 </tr>
 
@@ -79,10 +81,10 @@
             
                         <tr class="bg-primary my-3 rounded">
 
-                            <th class="p-3">{{ $daysOfWeek[$i] }}</th>
-                            <th class="p-3">COP {{ number_format($subTotal[$i], 0) }}</th>
-                            <th class="p-3">COP {{ number_format($growth[$i], 0) }}</th>
-                            <th class="p-3">{{ number_format($growthRate[$i], 2) }}%</th>
+                            <th class="p-3" id="dayofweek">{{ $daysOfWeek[$i] }}</th>
+                            <th class="p-3" id="subtotal">COP {{ number_format($subTotal[$i], 0) }}</th>
+                            <th class="p-3" id="grow">COP {{ number_format($growth[$i], 0) }}</th>
+                            <th class="p-3" id="previusgrowth">{{ number_format($growthRate[$i], 2) }}%</th>
                            
 
                         </tr>
