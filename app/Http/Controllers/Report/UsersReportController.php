fef0b6c40f09e2e3f174ef17e05f8c657a944025
Juan Pablo <juanpapabon@gmail.com>
2022-05-15 21:52:20 -0500

    refactor: 💡 implement the repository desing pattern

--- app/Http/Controllers/Report/UsersReportController.php
+++ app/Http/Controllers/Report/UsersReportController.php
@@ -4,35 +4,32 @@ namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GenerateReportRequest;
-use App\Models\User;
+use App\Repositories\User\UserRepository;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
-use Spatie\Permission\Models\Role;
 
 class UsersReportController extends Controller
 {
+    protected $usersRepo;
+
+    public function __construct(UserRepository $usersRepository)
+    {
+        $this->usersRepo = $usersRepository;
+    }
+
     public function index(): View
     {
         $initialDate = Carbon::now()->format('Y-m-d');
         return view('report.users.index', compact('initialDate'));
     }
 
-    public function generate(GenerateReportRequest $request)
+    public function generate(GenerateReportRequest $request): Response
     {
-        $users = User::whereDate('created_at', '>=', $request->get('initial-date'))
-            ->whereDate('created_at', '<=', $request->get('end-date'))->orderBy('created_at', 'Asc')
-            ->get(['id', 'name', 'surname', 'identification', 'address', 'phone', 'email', 'created_at']);
+        $users = $this->usersRepo->usersSearch($request);
 
-        $i = 0;
-        $roles[] = null;
-        foreach ($users as $user) {
-            $roleOfModel = DB::table('model_has_roles')->where('model_id', $user->id)->first();
-            $role = Role::where('id', $roleOfModel->role_id)->first();
-            $roles[$i] = $role->name;
-            $i++;
-        }
+        $roles = $this->usersRepo->rolesSearch($users);
 
         $pdf = PDF::loadView('report.users.usersReport', compact('users', 'roles'));
         return $pdf->stream();
