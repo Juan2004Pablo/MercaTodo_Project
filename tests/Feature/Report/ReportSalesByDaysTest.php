249c51413961ef07b88f57ecdaa18ee6bace6b93
Juan Pablo <juanpapabon@gmail.com>
2022-05-23 00:26:29 -0500

    test: 💍 tests to generate reports of sales by days

---
+++ tests/Feature/Report/ReportSalesByDaysTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Report;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ReportSalesByDaysTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index_sales_by_days_report(): void
+    {
+        $this->seed();
+        $user = $this->user();
+
+        $response = $this->actingAs($user)->get(route('salesByDays.report.index'));
+
+        $response->assertOk();
+    }
+
+    public function test_generate_sales_by_days_report(): void
+    {
+        $this->seed();
+        $user = $this->user();
+        $category = Category::factory()->create();
+        $products = Product::factory(8)->create(['category_id' => $category->id]);
+
+        $response = $this->actingAs($user)->post(route('salesByDays.report.generate', [
+            'initialDate' => now(),
+            'endDate' => now(),
+        ]));
+
+        $response->assertStatus(419);
+    }
+    
+    private function user(): User
+    {
+        $user = User::factory()->create();
+        $user->assignRole('FullAdmin');
+
+        return $user;
+    }
+}
