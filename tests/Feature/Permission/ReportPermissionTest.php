03f2dc373e27f085884e4c2388ce7cde528be7d2
Juan Pablo <juanpapabon@gmail.com>
2022-05-23 00:41:23 -0500

    test: 💍 tests to prove the ACL permissions about reports

---
+++ tests/Feature/Permission/ReportPermissionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature\Permission;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Constants\Resource;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+
+class ReportPermissionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index_reports(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('report.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_products_report(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('products.report.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_dailySales_report(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('dailySales.report.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_salesByDays_report(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('salesByDays.report.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_monthlySales_report(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('monthlySales.report.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_users_report(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('users.report.index'));
+
+        $response->assertForbidden();
+    }
+}
