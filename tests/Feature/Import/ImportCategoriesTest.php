a4aec1bcdde218347612e88c4aa848a24c2ba798
Juan Pablo <juanpapabon@gmail.com>
2022-05-23 00:21:06 -0500

    test: 💍 tests to import categories

---
+++ tests/Feature/Import/ImportCategoriesTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Import;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ImportCategoriesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_cannot_import_categories(): void
+    {
+        $this->seed();
+        $user = $this->user();
+        
+        $response = $this->actingAs($user)->post(route('categories.import', []));
+
+        $response->assertStatus(419);
+    }
+    
+    private function user(): User
+    {
+        $user = User::factory()->create();
+        $user->assignRole('FullAdmin');
+
+        return $user;
+    }
+}
